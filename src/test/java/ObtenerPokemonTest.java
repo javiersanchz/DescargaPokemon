c6f569fce4eaef8a9ee26cfb96d3c279fd0dd028
AlumnoM_Sanchez <javitosanchz32@gmail.com>
2022-04-27 10:33:54 +0200

    27/04/22

--- src/test/java/ObtenerPokemonTest.java
+++ src/test/java/ObtenerPokemonTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ObtenerPokemonTest {
@@ -13,33 +14,51 @@ public class ObtenerPokemonTest {
 
     @Test
     public void probarPokemon3Nombre(){
-        // TODO Comprueba que al preguntar por Pokémon con id 3 obtienes a venasaur.
+        // TODO Comprueba que al preguntar por Pokémon con id 3 obtienes a venusaur.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getName(),"venusaur");
     }
 
     @Test
     public void probarPokemon3Id(){
         // TODO Comprueba que al preguntar por Pokémon con venasaur obtienes el id 2.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getId(),3);
     }
 
     @Test
     public void probarPokemon3Peso(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes el peso de 1000.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getWeight(),1000);
     }
 
     @Test
     public void probarPokemon3Altura(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes la altura de 20.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getHeight(),20);
     }
 
     @Test
     public void probarPokemon4a6(){
         // TODO Comprueba que los Pokémon (id = 4, id = 5, id = 6) obtienes a charmander, charmeleon y charizard.
+        Pokemon pokemon4 = ObtenerPokemonRequest.Companion.get(4);
+        Pokemon pokemon5 = ObtenerPokemonRequest.Companion.get(5);
+        Pokemon pokemon6 = ObtenerPokemonRequest.Companion.get(6);
+        Assertions.assertEquals(pokemon4.getId(),4);
+        Assertions.assertEquals(pokemon5.getId(),5);
+        Assertions.assertEquals(pokemon6.getId(),6);
+
     }
 
     @Test
     public void probarPokemon25NombreEId(){
         // TODO Comprueba que cuando descargas un Pokémon con el id 25 obtienes un nombre, si descargas ese Pokémon por nombre obtienes un id.
         //  Comprueba que ambos Pokémon coinciden (el que has obtenido mediante el nombre y el que has obtenido mediante el id).
+        Pokemon pokemon25 = ObtenerPokemonRequest.Companion.get(25);
+        System.out.println(pokemon25.getName());
+
     }
 
     @Test
